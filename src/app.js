c9fca5112c40e11677068284e2a24f15e81acda3
yesus011 <jesu8907@gmail.com>
2024-10-18 17:41:52 +0000

    Jesus-Bolivar-Generador-de-cartas-aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,39 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let numberCard = ["2", "3", "4", "5", "6", "7", "8", "9", "10"];
+  let sticks = ["♦", "♥", "♠", "♣"];
+  let numberRandom = Math.floor(Math.random() * numberCard.length) + 2;
+  let sticksRandom = Math.floor(Math.random() * sticks.length);
+
+  let card = document.querySelector("#root");
+  let divTop = document.createElement("div");
+  let divMiddle = document.createElement("div");
+  let divBottom = document.createElement("div");
+
+  // card
+  card.classList.add("card");
+  card.appendChild(divTop);
+  card.appendChild(divMiddle);
+  card.appendChild(divBottom);
+
+  // top
+  divTop.textContent = sticks[sticksRandom];
+  divTop.classList.add("sticks");
+  if (sticks[sticksRandom] === "♦" || sticks[sticksRandom] === "♥") {
+    divTop.classList.add("red");
+  }
+
+  // middle
+  divMiddle.textContent = numberRandom;
+  divMiddle.classList.add("number");
+
+  // Bottom
+  divBottom.textContent = sticks[sticksRandom];
+  divBottom.classList.add("sticks");
+  divBottom.classList.add("stickDown");
+  if (sticks[sticksRandom] === "♦" || sticks[sticksRandom] === "♥") {
+    divBottom.classList.add("red");
+  }
 };
